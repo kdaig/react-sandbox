5d1dd9c0612d9f6742e4b71d420ad472a2b26b95
kdaig <kyli.chick@gmail.com>
2019-06-17 20:37:07 -0500

    rm dirs
    rm dirs

--- react-tut/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-var hw1 = React.createElement('h3', null, 'Hello World');
-var hw2 = React.createElement('h3', null, 'Pleasure to meet you');
-var div1 = React.createElement('div', null, hw1, hw2);
-ReactDOM.render(div1, document.getElementById('app'));
